89aba199247387d6cc27c4d446b51d0654855957
chomnr <zeljkovranjes@outlook.com>
2023-08-23 13:57:25 -0400

    test.

--- src/routes/TaskBar.svelte
+++ src/routes/TaskBar.svelte
@@ -7,7 +7,7 @@
 <div class="win7-taskbar win7-taskbar--aero">
 	<div class="win7-taskbar__content win7-taskbar__content--use-flex">
 		<div class="win7-taskbar__group win7-taskbar__group--use-remaining-space taskbar__group--horizontal-padding">
-			<div class="win7-taskbar__program win7-taskbar__program--startmenu"></div>
+			<div class="win7-taskbar__program win7-taskbar__program--startmenu">60000</div>
 		</div>
 		<div class="win7-taskbar__group win7-taskbar__group--far-right win7-taskbar__group--small-gap win7-taskbar__group--align-programs taskbar__group--horizontal-padding">
 			<div class="win7-taskbar__program win7-taskbar__program--list-programs font-xs"></div>

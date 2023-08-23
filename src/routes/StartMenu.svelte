da69f93846a95bb7258292fa54dd1588fa894273
chomnr <zeljkovranjes@outlook.com>
2023-08-23 18:27:15 -0400

    added icon to search input.

--- src/routes/StartMenu.svelte
+++ src/routes/StartMenu.svelte
@@ -2,6 +2,7 @@
     <div class="win7-startmenu__content win7-startmenu__content--with-design">
         <div class="win7-startmenu__group win7-startmenu__group--small win7-startmenu__group--default-design win7-startmenu__group--light-blue">
             <input class="font-xs font-italic" placeholder="Search programs and files"/>
+            <div class="input-icon"></div>
         </div>
     </div>
 </div>

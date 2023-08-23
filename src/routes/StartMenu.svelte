85a8871f693c40c1114500d3397921bb58c78689
chomnr <zeljkovranjes@outlook.com>
2023-08-23 17:40:41 -0400

    added input.

--- src/routes/StartMenu.svelte
+++ src/routes/StartMenu.svelte
@@ -1,7 +1,7 @@
 <div class="win7-startmenu win7--aero win7-startmenu--aero--additives">
     <div class="win7-startmenu__content win7-startmenu__content--with-design">
         <div class="win7-startmenu__group win7-startmenu__group--small win7-startmenu__group--default-design win7-startmenu__group--light-blue">
-            <input placeholder="Search programs and files" />
+            <input class="font-xs font-italic" placeholder="Search programs and files"/>
         </div>
     </div>
 </div>

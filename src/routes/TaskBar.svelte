5404a9c2f5326f0a7916d1c222ef4d3ae1ecc33d
chomnr <zeljkovranjes@outlook.com>
2023-08-23 12:33:47 -0400

    added network icons redid startmenu image.

--- src/routes/TaskBar.svelte
+++ src/routes/TaskBar.svelte
@@ -5,7 +5,14 @@
 </script>
 
 <div class="win7-taskbar win7-taskbar--aero">
-	<div class="win7-taskbar__content">
-		<div class="win7-taskbar__program win7-taskbar__program--startmenu"></div>
+	<div class="win7-taskbar__content win7-taskbar__content--use-flex">
+		<div class="win7-taskbar__group win7-taskbar__group--use-remaining-space taskbar__group--horizontal-padding">
+			<div class="win7-taskbar__program win7-taskbar__program--startmenu"></div>
+		</div>
+		<div class="win7-taskbar__group win7-taskbar__group--far-right win7-taskbar__group--small-gap win7-taskbar__group--align-programs taskbar__group--horizontal-padding">
+			<div class="win7-taskbar__program win7-taskbar__program--list-programs font-xs"></div>
+			<div class="win7-taskbar__program win7-taskbar__program--connection"></div>
+			<div class="win7-taskbar__program win7-taskbar__program--network-audio-3"></div>
+		</div>
 	</div>
 </div>

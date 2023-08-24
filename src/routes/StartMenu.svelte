c2e8327a2e1f6224c5a4b8d9c7c556db7565bbd8
chomnr <zeljkovranjes@outlook.com>
2023-08-23 22:24:21 -0400

    added start menu tabs.

--- src/routes/StartMenu.svelte
+++ src/routes/StartMenu.svelte
@@ -5,7 +5,7 @@
             <div>adsdas</div>
         </div>
 
-        <div class="win7-startmenu__group win7-startmenu__group--default-design align-center justify-left x-semi-large y-small font-small" style="width: 100pp;">
+        <div class="win7-startmenu__group win7-startmenu__group--default-design align-center justify-left x-semi-large y-small font-small">
             ⏵&nbsp;&nbsp;&nbsp; All Programs
         </div>
         
@@ -17,9 +17,47 @@
         </div>
     </div>
     <div class="win7-startmenu__content win7-startmenu__group--column">
-        <div class="win7-startmenu__group win7-startmenu__group--column win7-startmenu__group--reset-position color-white">
-            <!---->
-           
+        <div class="win7-avatar-builder" style="top:-50px;left:0px;" >
+            <img class="win7-avatar" src="/src/lib/images/windows/avatar/avatar.png" alt="avatar" />
+            <img class="win7-frame" src="/src/lib/images/windows/avatar/frame.png" alt="frame" />
+        </div>
+        <div class="win7-startmenu__group win7-startmenu__group--column justify-left x-fit y-fit font-small" style="margin:5px;height: 700px;position:relative;top:11px;">
+
+            <div class="aero-button">
+                <button>Zeljko</button>
+            </div>
+            <div class="aero-button">
+                <button>Documents</button>
+            </div>
+            <div class="aero-button">
+                <button>Pictures</button>
+            </div>
+            <div class="aero-button">
+                <button>Music</button>
+            </div>
+            <div class="aero-button">
+                <button>Games</button>
+            </div>
+            <div class="aero-button">
+                <button>Computer</button>
+            </div>
+            <div class="aero-button">
+                <button>Control Panel</button>
+            </div>
+            <div class="aero-button">
+                <button>Device and Printers</button>
+            </div>
+            <div class="aero-button">
+                <button>Default Programs</button>
+            </div>
+            <div class="aero-button">
+                <button>Help and Support</button>
+            </div>
+
+            <!--
+            <div class="aero-button" style="position:relative;top: 25px;width:fit-content;">
+                <button class="shutdown">NOPE</button>
+            </div>-->
         </div>
     </div>
 </div>

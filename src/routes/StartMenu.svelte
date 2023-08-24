5da395b1de5f654ae9193cc9383ee588a3b2a67d
chomnr <zeljkovranjes@outlook.com>
2023-08-23 20:36:36 -0400

    refractored.

--- src/routes/StartMenu.svelte
+++ src/routes/StartMenu.svelte
@@ -1,8 +1,22 @@
 <div class="win7-startmenu win7--aero win7-startmenu--aero--additives">
     <div class="win7-startmenu__content win7-startmenu__content--with-design">
-        <div class="win7-startmenu__group win7-startmenu__group--small win7-startmenu__group--default-design win7-startmenu__group--light-blue">
-            <input class="font-xs font-italic" placeholder="Search programs and files"/>
+        <div class="win7-startmenu__group win7-startmenu__group--default-design win7-startmenu__group--column y-inherit">
+            <div>adsdas</div>
+            <div>adsdas</div>
+        </div>
+
+        <div class="win7-startmenu__group win7-startmenu__group--default-design align-center x-semi-large y-small font-small" style="width: 100pp;">
+            ⏵&nbsp;&nbsp;&nbsp; All Programs
+        </div>
+        
+        <div class="win7-startmenu__group win7-startmenu__group--default-design win7-startmenu__group--light-blue y-medium">
             <div class="input-icon"></div>
+        </div>
+    </div>
+    <div class="win7-startmenu__content win7-startmenu__group--column">
+        <div class="win7-startmenu__group win7-startmenu__group--column win7-startmenu__group--reset-position color-white">
+            <!---->
+           
         </div>
     </div>
 </div>

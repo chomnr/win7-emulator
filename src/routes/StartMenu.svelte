d32b45ff013d79c93db04f6a127696022be6f4f0
chomnr <zeljkovranjes@outlook.com>
2023-08-23 20:41:09 -0400

    small tweaks with justify.

--- src/routes/StartMenu.svelte
+++ src/routes/StartMenu.svelte
@@ -1,16 +1,16 @@
 <div class="win7-startmenu win7--aero win7-startmenu--aero--additives">
     <div class="win7-startmenu__content win7-startmenu__content--with-design">
-        <div class="win7-startmenu__group win7-startmenu__group--default-design win7-startmenu__group--column y-inherit">
+        <div class="win7-startmenu__group win7-startmenu__group--default-design justify-left win7-startmenu__group--column y-inherit">
             <div>adsdas</div>
             <div>adsdas</div>
         </div>
 
-        <div class="win7-startmenu__group win7-startmenu__group--default-design align-center x-semi-large y-small font-small" style="width: 100pp;">
+        <div class="win7-startmenu__group win7-startmenu__group--default-design align-center justify-left x-semi-large y-small font-small" style="width: 100pp;">
             ⏵&nbsp;&nbsp;&nbsp; All Programs
         </div>
         
-        <div class="win7-startmenu__group win7-startmenu__group--default-design win7-startmenu__group--light-blue y-medium">
-            <div class="input-icon"></div>
+        <div class="win7-startmenu__group win7-startmenu__group--default-design align-center win7-startmenu__group--light-blue y-medium">
+            <input class="aero-input" placeholder="Search programs and files"/>
         </div>
     </div>
     <div class="win7-startmenu__content win7-startmenu__group--column">

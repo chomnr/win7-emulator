b783491ce8c1e6b84eb40f60c814c6ad5817883b
chomnr <zeljkovranjes@outlook.com>
2023-08-24 03:20:29 -0400

    added hover to network icons.

--- src/routes/components/TaskBar.svelte
+++ src/routes/components/TaskBar.svelte
@@ -1,5 +1,7 @@
-<script>
+<script lang="ts">
 		import { TogglableStartMenu } from "../stores";
+
+		var whitelist: string[] = ["win7-startmenu__content", "win7-startmenu__group"];
 
 		function toggleStartMenu(){
 			if ($TogglableStartMenu == false) {
@@ -8,9 +10,11 @@
 				TogglableStartMenu.set(false);
 			}
 		}
+
+		/*Close immediately when click out....*/
 </script>
 
-<div id="win7_btn_startmenu" class="win7-taskbar win7--aero win7-taskbar--aero--additives">
+<div class="win7-taskbar win7--aero win7-taskbar--aero--additives">
 	<div class="win7-taskbar__content win7-taskbar__content--use-flex">
 		<div class="win7-taskbar__group win7-taskbar__group--use-remaining-space taskbar__group--horizontal-padding">
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -22,8 +26,8 @@
 		</div>
 		<div class="win7-taskbar__group win7-taskbar__group--far-right win7-taskbar__group--small-gap win7-taskbar__group--align-programs taskbar__group--horizontal-padding">
 			<div class="win7-taskbar__program win7-taskbar__program--list-programs font-xs"></div>
-			<div class="win7-taskbar__program win7-taskbar__program--connection"></div>
-			<div class="win7-taskbar__program win7-taskbar__program--network-audio-3"></div>
+			<div class="win7-taskbar__program win7-taskbar__program--with-border win7-taskbar__program--hover win7-taskbar__program--connection"></div>
+			<div class="win7-taskbar__program win7-taskbar__program--with-border win7-taskbar__program--hover win7-taskbar__program--network-audio-3"></div>
 			<div class="win7-taskbar__program win7-taskbar__program--time win7-taskbar__program--fit-content win7-taskbar__program--time--left-padding font-segou_ui font-xs white">
 				<div>11:44 AM</div>
 				<div>9/22/2018</div>

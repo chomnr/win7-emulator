9f69e09303348354d1299127d98bad6773994c6c
chomnr <zeljkovranjes@outlook.com>
2023-08-25 02:12:27 -0400

    changed hover colors.

--- src/routes/components/DesktopGrid.svelte
+++ src/routes/components/DesktopGrid.svelte
@@ -4,7 +4,7 @@
         <div class="win7-desktop-grid__program--title">Internet Explorer</div>
     </div>
     <div class="win7-desktop-grid__program">
-        <div class="win7-desktop-grid__program--icon win7-desktop-grid__program--icon--explorer"></div>
+        <div class="win7-desktop-grid__program--icon win7-desktop-grid__program--icon--test"></div>
         <div class="win7-desktop-grid__program--title">Recycle Bin</div>
     </div>
     <div class="win7-desktop-grid__program">
@@ -28,7 +28,7 @@
         <div class="win7-desktop-grid__program--title">Recycle Bin</div>
     </div>
     <div class="win7-desktop-grid__program">
-        <div class="win7-desktop-grid__program--icon win7-desktop-grid__program--icon--explorer"></div>
+        <div class="win7-desktop-grid__program--icon win7-desktop-grid__program--icon--test"></div>
         <div class="win7-desktop-grid__program--title">Recycle Bin</div>
     </div>
 </div>

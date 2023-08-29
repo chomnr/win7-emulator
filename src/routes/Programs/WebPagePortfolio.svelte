380075db5b443ce50aac958738d25babf6a1899d
chomnr <zeljkovranjes@outlook.com>
2023-08-29 01:32:53 -0400

    added a little command aesthetic.

--- src/routes/Programs/WebPagePortfolio.svelte
+++ src/routes/Programs/WebPagePortfolio.svelte
@@ -1,47 +1,82 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import '../webpage.css';
-    import {CurrentDate} from "../stores"
-    import { LEET } from "../effects"
+    import { onMount } from "svelte";
+    import "../webpage.css";
+    import { LEET } from "../effects";
+
+    var div_command: HTMLElement | null;
+    var div_command_input: HTMLElement | null;
 
     var leet_is_solved: Boolean = false;
     onMount(() => {
+        div_command = document.getElementById("div_command");
+        //@ts-ignore
+        div_command.innerText = "execute_command";
+        div_command_input = document.getElementById("div_command_input");
+
+        function ChangeCommandInput(input: string) {
+            if (div_command_input != null) {
+                div_command_input.style.opacity = "0";
+                setTimeout(function () {
+                    //@ts-ignore 
+                    div_command_input.innerText = input;
+
+                    //@ts-ignore
+                    div_command_input.style.opacity = "1";
+                }, 500);
+            }
+        }
+
         function ApplyLeetEffect() {
             let leet_title: LEET = new LEET("branding_title", 50, 50); // 50,50 perfect sync with 100,30
-            let leet_position: LEET = new LEET("branding_position", 100, 30);;  //100, 30 perfect sync with 50 /50
+            let leet_position: LEET = new LEET("branding_position", 100, 30); //100, 30 perfect sync with 50 /50
 
             leet_title.ApplyEffect();
             leet_position.ApplyEffect();
-            
+
             setTimeout(() => {
                 leet_is_solved = true;
-            }, 3000);
+                ChangeCommandInput("unlock_virtual_terminal");
+            }, 4050);
         }
 
         function ApplyInitalWhite() {}
 
         ApplyLeetEffect();
         ApplyInitalWhite();
-    })
+    });
 </script>
-
 
 <div class="webpage">
     <div class="branding">
-        <div id="branding_title" class="title">ZELJKO VRANJES</div>
-        <div id="branding_position" class="position">FULL STACK ENGINEER</div>
+        <div class="group column">
+            <div id="branding_title" class="title">ZELJKO VRANJES</div>
+            <div id="branding_position" class="position">
+                FULL STACK ENGINEER
+            </div>
+        </div>
+        <div class="group float-right flex-1">
+            <div style="margin-left: auto;">
+                <div class="code-font" style="display:flex;">
+                    <div id="div_command">execute_command</div>
+                    (<span style="color: #00FF00;">'</span><span id="div_command_input" style="color: #00FF00;"
+                        >decrypt_credentials</span
+                    ><span style="color: #00FF00;">'</span>)
+                </div>
+            </div>
+        </div>
     </div>
     {#if leet_is_solved}
-    <div class="socials">
-        <i class="icon github animate"></i>
-        <i class="icon x animate" style="animation-duration: 2s"></i>
-        <i class="icon ycombinator animate" style="animation-duration: 3s"></i>
-    </div>
+        <div class="socials">
+            <i class="icon github animate" />
+            <i class="icon x animate" style="animation-duration: 2s" />
+            <i
+                class="icon ycombinator animate"
+                style="animation-duration: 3s"
+            />
+        </div>
     {/if}
 
-    <div class="begin-screen">
-        
-    </div>
+    <div class="begin-screen" />
     <!--
     <div class="body">
         asddsa

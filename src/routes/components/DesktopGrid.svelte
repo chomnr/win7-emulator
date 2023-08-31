087fadf6658d4f31907c684a72f6721fb8e8be4b
chomnr <zeljkovranjes@outlook.com>
2023-08-31 05:12:01 -0400

    added dynamic programs to desktop.

--- src/routes/components/DesktopGrid.svelte
+++ src/routes/components/DesktopGrid.svelte
@@ -74,6 +74,13 @@
 	});
 </script>
 <div id="desktop_grid" class="win7-desktop-grid">
+    {#each filter.GetPrograms() as program}
+        <div id="{program.GetFullIdentifier().string()}" class="win7-desktop-grid__program">
+            <div id="{program.GetIcon().string()}" class="win7-desktop-grid__program--icon win7-desktop-grid__program--icon--explorer"></div>
+            <div id="{program.GetTitle().string()}" class="win7-desktop-grid__program--title">{program.GetName()}</div>
+        </div>
+    {/each}
+    <!--
     <div id="program_ie9" class="win7-desktop-grid__program">
         <div id="program_ie9-icon" class="win7-desktop-grid__program--icon win7-desktop-grid__program--icon--explorer"></div>
         <div id="program_ie9-title" class="win7-desktop-grid__program--title">Internet<br>Explorer</div>
@@ -82,6 +89,6 @@
     <div id="program_test" class="win7-desktop-grid__program" style="z-index: 1;">
         <div id="program_test-icon" class="win7-desktop-grid__program--icon win7-desktop-grid__program--icon--test"></div>
         <div id="program_test-title" class="win7-desktop-grid__program--title">Recycle  adsasdadsdasadsas</div>
-    </div>
-    
+    </div>-->
+
 </div>

3b7aa0fb20f4551c4ac91d64c82e4d6052dde43e
chomnr <zeljkovranjes@outlook.com>
2023-09-02 00:35:25 -0400

    added ChangeDisplays and ChangeDisplay.

--- src/helper.ts
+++ src/helper.ts
@@ -23,8 +23,31 @@ export function RemoveRuleFrom(elements: HTMLElement[], rule: string) {
  * @param elements 
  * @param display 
  */
-export function ChangeDisplay(elements: HTMLElement[], display: string) {
+export function ChangeDisplays(elements: HTMLElement[], display: string) {
     for (let i = 0; i < elements.length; i++) {
         elements[i].style.display =  display;
+    }
+}
+
+/**
+ * Change the display of a single HTMLElement.
+ * @param target 
+ * @param display 
+ */
+export function ChangeDisplay(target: HTMLElement, display: string) {
+    target.style.display = display;
+}
+
+/**
+ * Update two classes simultaneously with a specific rule.
+ * @param target The element you want the rule applied to.
+ * @param removal The element you want the rule removed from.
+ */
+export function AddAndRemoveRule(target: HTMLElement, removal: HTMLElement, rule: string) {
+    if (target != removal) {
+        target.classList.add(rule);
+        removal.classList.remove(rule);
+    } else {
+        target.classList.add(rule);
     }
 }

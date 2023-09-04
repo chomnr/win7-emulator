521e3b6c0674e4d67376b15090a53682a141d974
chomnr <zeljkovranjes@outlook.com>
2023-09-04 03:28:44 -0400

    removed jquery added custom draggable.

---
+++ src/routes/components/Draggable.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+    import type { ComputerProgram } from "../../programs";
+    import { ActiveWindows, CurrentWindow } from "../stores";
+
+    export let left = 100;
+    export let top = 100;
+
+    export let program: ComputerProgram;
+
+    let moving = false;
+
+    /**
+     * 
+     */
+    function AdjustPriority() {
+        if ($CurrentWindow != program && $ActiveWindows.length > 1) {
+            $CurrentWindow.GetWindow().html().style.zIndex = "4";
+        }
+
+        if ($CurrentWindow == program) {
+            $CurrentWindow.GetWindow().html().style.zIndex = "5";
+        }
+    }
+
+    function onMouseDown(e: MouseEvent) {
+        // initial
+        if ($CurrentWindow == undefined) {
+            CurrentWindow.set(program);
+        }
+
+        if (e.target != null) {
+            var id = e.target.id;
+            var isHandle = id == program.GetHandle().string();
+
+            if (isHandle) {
+                moving = true;
+                AdjustPriority();
+            }
+        }
+    }
+
+    function onMouseMove(e) {
+        if (moving) {
+            left += e.movementX;
+            top += e.movementY;
+        }
+    }
+
+    function onMouseUp() {
+        moving = false;
+        CurrentWindow.set(program);
+    }
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<section
+    on:mousedown={onMouseDown}
+    style="left: {left}px; top: {top}px;"
+    class="draggable"
+>
+    <slot />
+</section>
+
+<svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
+
+<style>
+    .draggable {
+        user-select: none;
+        position: absolute;
+    }
+</style>
